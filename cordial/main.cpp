b41abd77d2fc56b5ff89d77b678067da4227129f
louis1001 <louis1001dev@gmail.com>
2022-02-08 22:57:08 -0600

    CLI: Añade soporte básico para argumentos de comando
    Recibe el nombre de un archivo, y lo ejecuta.

--- cordial/main.cpp
+++ cordial/main.cpp
@@ -6,31 +6,63 @@
 //
 
 #include <iostream>
-#include "lexer.hpp"
+#include <fstream>
+
+#include "parser.hpp"
+#include "ast_dump.h"
+#include "argh.h"
+
+void usage(const std::string& command = "cordial") {
+    std::cout << "Usage: \n"
+              << "\t" << command << " <archivo>\n"
+    ;
+}
 
 int main(int argc, const char * argv[]) {
-    // TODO: Debug position for tokens is not correct.
-    std::string code = R"(
-Esto no debe registrarse como un programa. Ni siquiera si escribo hola.
-La verdad, no se qué tan confuso pueda verse.
-    
-Código empezaría aquí:
-hola. porfavor
-    muestra "Hola, mundo"
-  y muestra 20,
-gracias. adios.
+    auto cmdl = argh::parser(argc, argv);
 
-Y esto estaría fuera de código. El programa termina luego del primer `adios`.
-)";
-    Cordial::Lexer lx;
-    
-    lx.setup(code, "anonymus");
-    
-    auto tk = lx.get_next();
-    while (tk.has_value()) {
-        std::cout << tk.value().get_str() << "\n";
-        tk = lx.get_next();
+    if (cmdl[{"-h", "--help"}]) {
+        usage();
+        return 0;
     }
-    
+
+    std::string filename;
+    cmdl(1) >> filename;
+
+    if (filename.empty()) {
+        std::cerr << "Falta nombre del archivo." << std::endl;
+        usage();
+        return 1;
+    }
+
+    std::cout << filename << "\n";
+
+    std::fstream source_file(filename);
+
+    std::string code((std::istreambuf_iterator<char>(source_file)),
+                     (std::istreambuf_iterator<char>()));
+
+    source_file.close();
+
+    // TODO: Debug position for tokens is not correct.
+
+    Cordial::Parser parser;
+    auto result = parser.parse(code, filename);
+
+    Cordial::ASTDumper visitor;
+    auto tree = visitor.visit(result, 0);
+
+    std::cout << tree;
+
+//    Cordial::Lexer lx;
+//
+//    lx.setup(code, "anonymus");
+//
+//    auto tk = lx.get_next();
+//    while (tk.has_value()) {
+//        std::cout << tk.value().get_str() << "\n";
+//        tk = lx.get_next();
+//    }
+
     return 0;
 }
